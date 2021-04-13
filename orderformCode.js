daf81beed08c59709f999f56a8c95bbfee126650
lynnsimonen <79551770+lynnsimonen@users.noreply.github.com>
2021-04-13 18:01:43 -0500

    Webpage looks good but formulas are not working???

--- orderformCode.js
+++ orderformCode.js
@@ -1,35 +1,34 @@
 $(document).ready(
-    function(){
-        $("input#learningQty").on("keypress",calculate);
-        $("input#donationQty").on("keypress",calculate);
+    function() {
+        $("#learningQty").on("keypress", learningQtyTotal);
+        $("#donationQty").on("keypress", donationQtyTotal);
 
-        // Include event parameter to get information about the event
-        // that triggered this function to run
-        function calculate() {
-            // Capture the quantity entered into textbox
+        let learningQty = parseFloat($("#learningQty").val());
+        let donationQty = parseFloat($("#donationQty").val());
 
-            let learningQty = parseFloat($("#learningQty").val());
-            let donationQty = parseFloat($("#donationQty").val());
+        function learningQtyTotal(event) {
 
-            //do some math...
-            //Learning jQuery Price = $39.99   ---  Query Donation Price = $14.99 (Round result to 2 decimal places)
             let learningTotal = (learningQty * 39.99);
+            $('#learningTotal').text(learningTotal);
+
+            let total = (donationQty * 14.99) + (learningQty*39.99);
+            $("p#total").text(total.toFixed(2));
+        }
+
+        function donationQtyTotal(event) {
+
             let donationTotal = (donationQty * 14.99);
+            $("p#donationTotal").text(donationTotal);
 
-            $("p#learningTotal").val();
-            $("p#donationTotal").val();
-
-            //OUTPUT
-            //Grand Total is sum of both products totals.
-
-            let Total = learningTotal + donationTotal;
-            $("p#Total").text(`${Total}`);
-            //$("p#Total").text(Total.toFixed(2));
-
-
-            $("body").css("background-color", "red");
-            //$(".product").css("color", "blue")
+            //let total = (donationQty*14.99) + (learningQty*39.99);
+            //$('#total').text(total.toFixed(2));
         }
     })
 
+            //$("p#donationTotal").text(donationTotal).css("font-weight","Bold");
+            //$("p#Total").text(Total.toFixed(2));
+            //$("body").css("background-color", "red");
+            //$(".product").css("color", "blue")
 
+// Capture the quantity entered into text box
+//let learningQty = parseFloat($("input#learningQty").val());
